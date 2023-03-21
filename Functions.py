f65d99e38d7c2d7c3ee40680ff766b1150e12168
Kamil-Krawiec <kamil@Kamils-MacBook-Pro.local>
2023-03-21 13:10:40 +0100

    changed function to print results (changes fixed) and taboo serach works fine

--- Functions.py
+++ Functions.py
@@ -1,5 +1,6 @@
 # class for Priority queue
 from datetime import timedelta
+from time import process_time
 
 import pandas as pd
 
@@ -9,23 +10,20 @@ from Classes import *
 def decode_path(came_from, goal, start, DEPTH, NODES):
     path = []
     current = goal
-    changes = set()
-    path.append([goal, 'end'])
+    changes = []
+    path.append([goal, (came_from[goal])[1].end_time.time()])
     while current != start:
         info = came_from[current]
         edge = info[1]
-        changes.add(edge.line)
+        if changes == [] or changes[-1] != edge.line: changes.append(edge.line)
         path.append([info[0], edge.start_time.time(), edge.end_time.time(), edge.line])
         current = info[0]
 
     time_diff = datetime.combine(date.today(), path[1][2]) - datetime.combine(date.today(), path[len(path) - 1][1])
-
     path.reverse()
 
     path_df = pd.DataFrame(path, columns=['start_stop', 'start_time', 'end_time', 'line'])
-
-    # print(path_df)
-    return path, path_df, len(changes), time_diff, DEPTH, NODES
+    return path, path_df, len(changes) - 1, time_diff, DEPTH, NODES
 
 
 # ------------------------- DIJKSTRA
@@ -86,7 +84,7 @@ def aStar_time(graph, start, goal, time):
     cost_so_far[start] = 0
 
     while not frontier.empty():
-        DEPTH+=1
+        DEPTH += 1
         current = frontier.get()
 
         current_time = time + timedelta(seconds=cost_so_far[current])
@@ -103,7 +101,7 @@ def aStar_time(graph, start, goal, time):
                 priority = new_cost + time_heuristic(graph.vertexes[goal], graph.vertexes[next_stop])
                 frontier.put(next_stop, priority)
                 came_from[next_stop] = [current, edge]
-            NODES+=1
+            NODES += 1
     return None
 
 
@@ -129,7 +127,7 @@ def aStar_transfers(graph, start, goal, time, n):
     cost_so_far[start] = 0
 
     while not frontier.empty():
-        DEPTH+=1
+        DEPTH += 1
         current = frontier.get()
 
         current_time = time + timedelta(seconds=time_delta[current])
@@ -151,13 +149,11 @@ def aStar_transfers(graph, start, goal, time, n):
 
                 priority = new_cost + transfer_heuristic(lines, edge.line)
 
-
                 frontier.put(next_stop, priority)
 
                 came_from[next_stop] = [current, edge]
-            NODES+=1
+            NODES += 1
     return None
-
 
 
 def calculate_b_star(N, d):
@@ -166,8 +162,8 @@ def calculate_b_star(N, d):
 
     for i in range(1000):
         sum_b = 0.0
-        for j in range(d+1):
-            sum_b += b_star**j
+        for j in range(d + 1):
+            sum_b += b_star ** j
 
         if abs(N - sum_b) < eps:
             return b_star
@@ -177,3 +173,147 @@ def calculate_b_star(N, d):
             b_star += 0.01
 
     return b_star
+
+
+# -------------------------------------------- FUNCTION FOR TESTING EX1
+def test_ex1(graph, start, goal, time):
+    path_list_first_ATR = aStar_time(graph, start=start, goal=goal, time=time)[1]
+    print(path_list_first_ATR)
+
+    # TRANSFER ASTAR
+    ATR = []
+    path_list_first_ATR = aStar_transfers(graph, start=start, goal=goal, time=time, n=8110)[0]
+
+    for i in range(100):
+        start_astar_transfer = process_time()
+        path_list_ATR, path_df_ATR, transfers_ATR, duration_ATR, D_ATR, N_ATR = aStar_transfers(graph, start=start,
+                                                                                                goal=goal, time=time,
+                                                                                                n=8110)
+        end_astar_transfer = process_time()
+        ATR.append([int(path_list_ATR == path_list_first_ATR), transfers_ATR, duration_ATR,
+                    end_astar_transfer - start_astar_transfer, calculate_b_star(N_ATR, D_ATR)])
+    df_ATR = pd.DataFrame(ATR, columns=['the_same_path', 'transfers', 'duration', 'time_of_eval', 'branching_factor'])
+
+    print("{} {} {}".format(30 * '#', "Transfer A", 30 * '#'))
+    print(df_ATR.describe().to_string())
+
+    # TIME ASTAR
+    ATI = []
+    path_list_first_ATI = aStar_time(graph, start=start, goal=goal, time=time)[0]
+
+    for i in range(100):
+        start_astar_time = process_time()
+        path_list_ATI, path_df_ATI, transfers_ATI, duration_ATI, D_ATI, N_ATI = aStar_time(graph, start=start,
+                                                                                           goal=goal,
+                                                                                           time=time)
+        end_astar_time = process_time()
+        ATI.append(
+            [int(path_list_ATI == path_list_first_ATI), transfers_ATI, duration_ATI, end_astar_time - start_astar_time,
+             calculate_b_star(N_ATI, D_ATI)])
+    df_ATI = pd.DataFrame(ATI, columns=['the_same_path', 'transfers', 'duration', 'time_of_eval', 'branching_factor'])
+    print("{} {} {}".format(30 * '#', "Time A", 30 * '#'))
+    print(df_ATI.describe().to_string())
+
+    # TIME DIJKSTRA
+    DI = []
+    path_list_first_DI = dijkstra_time(graph, start=start, goal=goal, time=time)[0]
+
+    for i in range(100):
+        start_dijkstra_time = process_time()
+        path_list_D, path_df_D, transfers_D, duration_D, D_D, N_D = dijkstra_time(graph, start=start, goal=goal,
+                                                                                  time=time)
+        end_dijkstra_time = process_time()
+        DI.append(
+            [int(path_list_D == path_list_first_DI), transfers_D, duration_D, end_dijkstra_time - start_dijkstra_time,
+             calculate_b_star(N_D, D_D)])
+    df_DI = pd.DataFrame(DI, columns=['the_same_path', 'transfers', 'duration', 'time_of_eval', 'branching_factor'])
+    print("{} {} {}".format(30 * '#', "Time D", 30 * '#'))
+    print(df_DI.describe().to_string())
+
+
+# -------------------------------- TABOO SEARCH
+# Definiujemy funkcję celu (może to być dowolna funkcja, którą chcemy zminimalizować)
+
+
+class Taboo:
+    def __init__(self, graph):
+        self.time = None
+        self.graph = graph
+
+    def decode_solution(self, best_solution):
+        path_sum = []
+        sum = 0
+        transfers = 0
+        for i in range(1, len(best_solution)):
+            path, _, changes, time_diff, _, _ = aStar_time(self.graph, best_solution[i - 1], best_solution[i],
+                                                     self.time + timedelta(seconds=sum))
+            path_sum.extend(path)
+            transfers += changes
+            sum += time_diff.total_seconds()
+
+        return pd.DataFrame(path_sum, columns=['start_stop', 'start_time', 'end_time', 'line']),transfers
+
+    def objective_function_time(self, solution):
+        sum = 0
+        for i in range(1, len(solution)):
+            _, _, _, time_diff, _, _ = aStar_time(self.graph, solution[i - 1], solution[i],
+                                                  self.time + timedelta(seconds=sum))
+            sum += time_diff.total_seconds()
+        return sum
+
+    def objective_function_transfers(self, solution):
+        sum = 0
+        transfers = 0
+        for i in range(1, len(solution)):
+            _, _, changes, time_diff, _, _ = aStar_transfers(self.graph, solution[i - 1], solution[i],
+                                                             self.time + timedelta(seconds=sum), n=8100)
+            sum += time_diff.total_seconds()
+            transfers += changes
+        return transfers
+
+    # Definiujemy funkcję generującą sąsiedztwo (zmiany w rozwiązaniu)
+    def get_neighbors(self, solution):
+        neighbors = [solution.copy()]
+        for i in range(1, len(solution) - 1):
+            for j in range(i + 1, len(solution) - 1):
+                neighbor = solution.copy()
+                neighbor[i], neighbor[j] = neighbor[j], neighbor[i]
+                neighbors.append(neighbor)
+
+        return neighbors
+
+    # Definiujemy algorytm Tabu Search
+    def taboo_search(self, initial_solution, max_iterations, time, criteria):
+        objective_function = self.objective_function_time if criteria == 't' else self.objective_function_transfers
+        self.time = pd.to_datetime(time)
+
+        current_solution = initial_solution.copy()
+
+        best_solution = current_solution.copy()
+
+        tabu_list = []
+
+        iteration = 0
+
+        while iteration < max_iterations:
+            neighbors = self.get_neighbors(current_solution)
+
+            neighbors = [n for n in neighbors if n not in tabu_list]
+
+            if not neighbors:
+                break
+
+            next_solution = min(neighbors, key=objective_function)
+
+            tabu_list.append(next_solution)
+
+            # if len(tabu_list) > tabu_list_size:
+            #     tabu_list.pop(0)
+
+            if objective_function(next_solution) < objective_function(best_solution):
+                best_solution = next_solution.copy()
+
+            current_solution = next_solution.copy()
+            iteration += 1
+        print(iteration)
+        return self.decode_solution(best_solution)
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from time import process_time
-
 from Functions import *
-
+from time import process_time
 # READING AND UNDERSTANDING DATA
 df = pd.read_csv('connection_graph.csv', low_memory=False, header=0)
 df = df.drop(['Unnamed: 0.1', 'Unnamed: 0'], axis=1)
@@ -31,59 +29,29 @@ for i, row in df.iterrows():
     graph.add_edge(row['start_stop'], row['end_stop'], edge)
 
 # --------------------------------------------------------- TESTING
-start = 'OPORÓW'
-goal = 'BISKUPIN'
-time = '17:00:00'
 
-path_list_first_ATR = aStar_time(graph, start=start, goal=goal, time=time)[1]
-print(path_list_first_ATR)
+taboo = Taboo(graph)
 
-# TRANSFER ASTAR
-ATR = []
-path_list_first_ATR = aStar_transfers(graph, start=start, goal=goal, time=time, n=8110)[0]
+start = process_time()
+best_result_transfers = taboo.taboo_search(
+    initial_solution = ['OPORÓW', 'DWORZEC GŁÓWNY', 'Kasprowicza','Grabiszyńska','Psie Pole', 'OPORÓW'],
+    max_iterations=100,
+    time='17:00:00',
+criteria='c'
+)
+end = process_time()
 
-for i in range(100):
-    start_astar_transfer = process_time()
-    path_list_ATR, path_df_ATR, transfers_ATR, duration_ATR, D_ATR, N_ATR = aStar_transfers(graph, start=start,
-                                                                                            goal=goal, time=time,
-                                                                                            n=8110)
-    end_astar_transfer = process_time()
-    ATR.append([int(path_list_ATR == path_list_first_ATR), transfers_ATR, duration_ATR,
-                end_astar_transfer - start_astar_transfer,calculate_b_star(N_ATR,D_ATR)])
-df_ATR = pd.DataFrame(ATR, columns=['the_same_path', 'transfers', 'duration', 'time_of_eval', 'branching_factor'])
+print(best_result_transfers[0].to_string())
+print("time of eval: {} transfers {}".format(end-start,best_result_transfers[1]))
 
-print("{} {} {}".format(30 * '#', "Transfer A", 30 * '#'))
-print(df_ATR.describe().to_string())
+start = process_time()
+best_result_time = taboo.taboo_search(
+    initial_solution = ['OPORÓW', 'DWORZEC GŁÓWNY', 'Kasprowicza','Grabiszyńska','Psie Pole', 'OPORÓW'],
+    max_iterations=100,
+    time='17:00:00',
+criteria='t'
+)
+end = process_time()
 
-# TIME ASTAR
-ATI = []
-path_list_first_ATI = aStar_time(graph, start=start, goal=goal, time=time)[0]
-
-for i in range(100):
-    start_astar_time = process_time()
-    path_list_ATI, path_df_ATI, transfers_ATI, duration_ATI, D_ATI, N_ATI = aStar_time(graph, start=start, goal=goal,
-                                                                                       time=time)
-    end_astar_time = process_time()
-    ATI.append(
-        [int(path_list_ATI == path_list_first_ATI), transfers_ATI, duration_ATI, end_astar_time - start_astar_time,
-         calculate_b_star(N_ATI,D_ATI)])
-df_ATI = pd.DataFrame(ATI, columns=['the_same_path', 'transfers', 'duration', 'time_of_eval', 'branching_factor'])
-print("{} {} {}".format(30 * '#', "Time A", 30 * '#'))
-print(df_ATI.describe().to_string())
-
-# TIME DIJKSTRA
-DI = []
-path_list_first_DI = dijkstra_time(graph, start=start, goal=goal, time=time)[0]
-
-for i in range(100):
-    start_dijkstra_time = process_time()
-    path_list_D, path_df_D, transfers_D, duration_D, D_D, N_D = dijkstra_time(graph, start=start, goal=goal, time=time)
-    end_dijkstra_time = process_time()
-    DI.append(
-        [int(path_list_D == path_list_first_DI), transfers_D, duration_D, end_dijkstra_time - start_dijkstra_time,
-         calculate_b_star(N_D, D_D)])
-df_DI = pd.DataFrame(DI, columns=['the_same_path', 'transfers', 'duration', 'time_of_eval', 'branching_factor'])
-print("{} {} {}".format(30 * '#', "Time D", 30 * '#'))
-print(df_DI.describe().to_string())
-
-
+print(best_result_time[0].to_string())
+print("time of eval: {} transfers {}".format(end-start,best_result_time[1]))
